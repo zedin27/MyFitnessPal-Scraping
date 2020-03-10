87ecf6a54abac1ebce92a06137d2827ffbd12a5b
Guillermo Villegas <gev14b@my.fsu.edu>
2020-03-09 17:02:31 -0700

    fixed comments and html.txt is now removed automatically

--- mfp_scraping.py
+++ mfp_scraping.py
@@ -1,8 +1,8 @@
+from selenium import webdriver
 from config import items
 import requests
-from selenium import webdriver
-import time
 import re
+import os
 
 def get_items(i):
 	#dict stores item names with their protein value
@@ -15,15 +15,18 @@ def get_items(i):
 		search_button = driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div/div[2]/div[2]/form/button')
 		search_button.click()
 		item_html = requests.get(driver.current_url)
-		html_file = open('html.txt', 'w')
+
+		#html.txt stores the html code of the currently parsed item from config
+		html_file = open("html.txt", 'w')
 		html_file.write(item_html.text)
-		html_file = open('html.txt')
+		html_file = open("html.txt")
 
 		for j in html_file.readlines():
-			match = re.search('Protein: (\d+)', j)
+			match = re.search("Protein: (\d+)", j)
 			if match:
 				dict[i] = match.group()
 	# print(dict)
+	os.remove("html.txt")
 
 if __name__ == "__main__":
 	get_items(items)
